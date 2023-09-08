435dd5e1e013ac1b4ee8647fa0993f4cc65e2679
bycloud <cloudunderscore2020@gmail.com>
2023-09-08 11:41:08 -0400

    Add files via upload

---
+++ pinokio.js
@@ -0,0 +1,9 @@
+module.exports = {
+  menu: [{
+    title: "Tokenflow",
+      description: "TokenFlow: Consistent Diffusion Features for Consistent Video Editing",
+      icon: "icon.png",
+    html: "<i class='fa-solid fa-plug'></i> Install",
+    href: "install.json"
+  }]
+}
